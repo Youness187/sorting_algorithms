f2584002db83a459ff3cab35951da3118a0be88b
younesse el-mouden <dayouness187@gmail.com>
2023-07-24 00:43:24 +0000

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,7 +7,7 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *k_arr, k, j, tmp;
+	int *k_arr, *tmp_arr, k, j;
 	size_t i = 0;
 
 	if (array == NULL || size < 2)
@@ -15,10 +15,9 @@ void counting_sort(int *array, size_t size)
 
 	k = array[i];
 	for (; i < size; i++)
-	{
 		if (array[i] > k)
 			k = array[i];
-	}
+
 
 	k_arr = malloc(sizeof(int) * (k + 1));
 	if (k_arr == NULL)
@@ -26,20 +25,25 @@ void counting_sort(int *array, size_t size)
 
 	for (j = 0; j <= k; j++)
 		k_arr[j] = 0;
-
 	for (i = 0; i < size ; i++)
 		k_arr[array[i]] += 1;
-
 	for (j = 1; j <= k; j++)
 		k_arr[j] += k_arr[j - 1];
-
 	print_array(k_arr, k + 1);
+
+	tmp_arr = malloc(size * sizeof(int));
+	if (tmp_arr == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
-		tmp = array[k_arr[array[i]] - 1];
-		array[k_arr[array[i]] - 1] = array[i];
-		array[i] = tmp;
+		tmp_arr[k_arr[array[i]] - 1] = array[i];
+		k_arr[array[i]] -= 1;
 	}
+
+	for (i = 0; i < size; i++)
+		array[i] = tmp_arr[i];
+
+	free(tmp_arr);
 	free(k_arr);
 }
